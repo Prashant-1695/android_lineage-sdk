014cd4e767704871f8160c3cd1d2886512453cd1
Prashant-1695 <prashant33968@gmail.com>
2022-11-25 06:46:32 +0530

    Revert "Battery light: 100% charged level (3/3)"
    This reverts commit 53611b587c02475af16e89d79f11d4395316afbc.

--- sdk/src/java/org/lineageos/internal/notification/LineageBatteryLights.java
+++ sdk/src/java/org/lineageos/internal/notification/LineageBatteryLights.java
@@ -53,7 +53,6 @@ public final class LineageBatteryLights {
     private int mBatteryLowARGB;
     private int mBatteryMediumARGB;
     private int mBatteryFullARGB;
-    private int mBatteryReallyFullARGB;
     private int mBatteryBrightnessLevel;
     private int mBatteryBrightnessZenLevel;
 
@@ -169,15 +168,9 @@ public final class LineageBatteryLights {
             }
         } else if (isChargingOrFull) {
             if (isCharged || level >= 90) {
-                if (level == 100) {
-                    // Battery is full
-                    ledValues.setColor(mBatteryReallyFullARGB);
-                    ledValues.setSolid();
-                } else {
-                    // Battery is nearly full
-                    ledValues.setColor(mBatteryFullARGB);
-                    ledValues.setSolid();
-                }
+                // Battery is full or charging and nearly full.
+                ledValues.setColor(mBatteryFullARGB);
+                ledValues.setSolid();
             } else {
                 // Battery is charging and not nearly full.
                 ledValues.setColor(mBatteryMediumARGB);
@@ -248,9 +241,6 @@ public final class LineageBatteryLights {
                 resolver.registerContentObserver(LineageSettings.System.getUriFor(
                         LineageSettings.System.BATTERY_LIGHT_FULL_COLOR), false, this,
                         UserHandle.USER_ALL);
-                resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                        LineageSettings.System.BATTERY_LIGHT_REALLY_FULL_COLOR), false, this,
-                        UserHandle.USER_ALL);
             }
 
             if (mCanAdjustBrightness) {
@@ -303,10 +293,6 @@ public final class LineageBatteryLights {
             mBatteryFullARGB = LineageSettings.System.getIntForUser(resolver,
                     LineageSettings.System.BATTERY_LIGHT_FULL_COLOR, res.getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryFullARGB),
-                    UserHandle.USER_CURRENT);
-            mBatteryReallyFullARGB = LineageSettings.System.getIntForUser(resolver,
-                    LineageSettings.System.BATTERY_LIGHT_REALLY_FULL_COLOR, res.getInteger(
-                    com.android.internal.R.integer.config_notificationsBatteryReallyFullARGB),
                     UserHandle.USER_CURRENT);
 
             // Adustable battery LED brightness.
